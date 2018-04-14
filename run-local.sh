14c3a80c8b2ca69cb215a5b70194478a21a65146
Naresh Rayapati <nrayapati@users.noreply.github.com>
2018-04-14 18:56:26 -0500

    Hubot Steps 2.0.0 Update (#1)

--- run-local.sh
+++ run-local.sh
@@ -4,7 +4,8 @@
 export PORT=9999
 export EXPRESS_PORT=9999
 export HUBOT_ADAPTER=slack
-export HUBOT_SLACK_TOKEN=SLACK-TOKEN
-export HUBOT_JENKINS_URL=http://localhost:9090
+export HUBOT_SLACK_TOKEN=<TOKEN>
+export HUBOT_JENKINS_URL=http://192.168.1.175:9091/
+export HUBOT_JENKINS_AUTH=chatops:09557eb3f90f502fbc305498ce350ff1
 
 ./bin/hubot
